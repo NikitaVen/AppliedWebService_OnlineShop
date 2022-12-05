7902516ca78999ea15bd586d734b859a95850f5b
AllerleiMal <emelpavel2002@gmail.com>
2022-12-06 02:05:43 +0300

    feat: add linking relationships between dbsets

--- OnlineShop/OnlineShop/Models/ShopContext.cs
+++ OnlineShop/OnlineShop/Models/ShopContext.cs
@@ -4,13 +4,42 @@ namespace OnlineShop.Models
 {
     public class ShopContext : DbContext
     {
-        public ShopContext() { }
+        public ShopContext()
+        {
+        }
 
-        public ShopContext(DbContextOptions<ShopContext> options) : base(options) 
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
         {
             Database.EnsureCreated();
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>()
+                .HasOne(a => a.Manufacturer)
+                .WithMany()
+                .HasForeignKey(i => i.ID_Manufacturer)
+                .IsRequired();
+            modelBuilder.Entity<Item>()
+                .Navigation(i => i.Manufacturer)
+                .UsePropertyAccessMode(PropertyAccessMode.Property)
+                .AutoInclude();
+
+            modelBuilder.Entity<Item>()
+                .HasOne(a => a.Item_Code)
+                .WithMany()
+                .HasForeignKey(i=>i.ItemCode);
+            
+            modelBuilder.Entity<Item>()
+                .Navigation(i => i.Item_Code)
+                .UsePropertyAccessMode(PropertyAccessMode.Property)
+                .AutoInclude()
+                .IsRequired();;
+            base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<Item> Items { get; set; }
+        public DbSet<Item_code> ItemCodes { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
     }
 }
