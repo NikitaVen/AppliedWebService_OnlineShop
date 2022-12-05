5c65f6c0cf6e26bf85fe3c4855b41dc05b7b8333
NikitaVen <nikben2105@gmail.com>
2022-12-06 02:13:09 +0300

    added: item grid, basket model

--- OnlineShop/OnlineShop/Controllers/ShopController.cs
+++ OnlineShop/OnlineShop/Controllers/ShopController.cs
@@ -7,16 +7,33 @@ namespace OnlineShop.Controllers
     public class ShopController : Controller
     {
         ShopContext context;
+        Basket basket;
 
         public ShopController(ShopContext context)
         {
             this.context = context;
+            this.basket = new Basket();
         }
 
         [HttpGet]
         public IActionResult Table()
         {
             return View(context.Items.ToList<Item>());
+        }
+
+        public void defaultBasketPut(string itemId)
+        {
+            Item chosenItem = context.Items.First(i => i.Id == Int64.Parse(itemId));
+            if (chosenItem.Amount > 0)
+            {
+                int amount;
+                if (basket.items.TryGetValue(chosenItem, out amount))
+                {
+                    basket.items.Add(chosenItem, amount + 1);
+                }
+                else
+                    basket.items.Add(chosenItem, 1);
+            }
         }
     }
 }
---
+++ OnlineShop/OnlineShop/Models/Basket.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineShop.Models
+{
+    public class Basket
+    {
+        public Dictionary<Item, int> items { get; set; }
+
+        public Basket()
+        {
+            items = new Dictionary<Item, int>(); 
+        }
+    }
+}
