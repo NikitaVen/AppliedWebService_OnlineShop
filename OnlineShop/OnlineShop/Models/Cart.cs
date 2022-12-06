f0a2eda49d20dcd1f815386d9100d6ed0acaa16c
NikitaVen <nikben2105@gmail.com>
2022-12-06 21:03:34 +0300

    scale bug

---
+++ OnlineShop/OnlineShop/Models/Cart.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineShop.Models
+{
+    public class Cart
+    {
+        public Dictionary<Item, int> items { get; set; }
+
+        public Cart()
+        {
+            items = new Dictionary<Item, int>();
+        }
+    }
+}
